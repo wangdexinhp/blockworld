0bf5e51cc0f47722b993344d3bde66ddd2f1d05c
wangdexin <wangdexin@baihai.ai>
2024-10-17 20:06:00 +0800

    update rabbit mq

--- demo/src/deref_test/rabbit_mq.rs
+++ demo/src/deref_test/rabbit_mq.rs
@@ -1,7 +1,25 @@
+use lapin::options::*;
+use lapin::types::FieldTable;
+use lapin::BasicProperties;
+use lapin::Channel;
+use lapin::Connection;
+use lapin::ConnectionProperties;
 
 #[tokio::test]
 pub async fn test_v1() {
     println!("00000")
 }
+
+
+pub struct RabbitMqClient(Channel);
+impl std::ops::Deref for RabbitMqClient {
+    type Target = Channel;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+
 
 
